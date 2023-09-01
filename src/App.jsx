f30419c5c0eb7d7becf474a4b47c443f2f06286d
Suman Mondal <sumanmondalmb71@gmail.com>
2023-09-01 22:44:04 +0530

    Part 3 - Starting File

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
+import React from "react";
 import Header from "./components/Header/Header";
 import Note from "./components/Note/Note";
 import Footer from "./components/Footer/Footer";
-import notes from "./notes";
+import CreateArea from "./components/Input/CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map((noteItem) => (
-        <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
-        />
-      ))}
+      <CreateArea />
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./footer.css"
 
-const currentYear = new Date().getFullYear()
+const year = new Date().getFullYear()
 
 const Footer = () => {
   return (
     <div>
       <footer>
-        <p>Copyright &copy;{currentYear}</p>
+        <p>Copyright ⓒ {year}</p>
       </footer>
     </div>
   )
